847d0ac304ad2ded3630482fe2a359f79900f454
tsalla <tilak.04@yahoo.com>
2022-01-21 19:21:12 +0530

    Adding lines and giving space in between words

--- 2-print.sh
+++ 2-print.sh
@@ -23,3 +23,9 @@ echo -e "\e[31mRED\e[32mGREEN\e[33mYELLOW\e[34mBLUE\e[35mMAGENTA\e[36mCYAN\e[0m"
 
 
 echo -e "\e[1;31mRED\e[1;32mGREEN\e[1;33mYELLOW\e[1;34mBLUE\e[1;35mMAGENTA\e[1;36mCYAN\e[0m"
+
+# There are two more esc seq generally we use in scripting
+# 1. New Line \n
+# 2. New Tab \t
+echo -e "Line1\nLine2"
+echo -e "WORD1\t\tWORD2"
