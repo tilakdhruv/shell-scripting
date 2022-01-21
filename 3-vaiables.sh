2b64b24ebf268fadf48170ffeb3cffa677ab5e10
tsalla <tilak.04@yahoo.com>
2022-01-21 19:29:15 +0530

    Adding variables 1 addition and 1 deletion

--- 3-vaiables.sh
+++ 3-vaiables.sh
@@ -5,3 +5,12 @@ student_name="Raju"
 
 echo student_name = $student_name
 echo student_name = ${student_name}
+
+
+#!/bin/bash
+
+student_name="Raju"
+student_name="John"
+
+echo student_name = $student_name
+echo student_name = ${student_name}
