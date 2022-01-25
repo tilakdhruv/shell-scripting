e91419f53f17936271df8c7bd0d1f92b168b7d0e
tsalla <tilak.04@yahoo.com>
2022-01-25 19:33:25 +0530

    Updating shell-scripting

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,10 +1,10 @@
 
-
-rm -f /tmp/roboshop.log
+LOGFILE=/tmp/roboshop.log
+rm -f $LOGFILE
 
 echo "Installing NGINX"
-yum install nginx -y >>/tmp/roboshop.log
+yum install nginx -y &>>$LOGFILE
 
 echo "Downloading Frontend Content"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-de
-vops-project/frontend/archive/main.zip" >>/tmp/roboshop.log
+vops-project/frontend/archive/main.zip" &>>$LOGFILE
