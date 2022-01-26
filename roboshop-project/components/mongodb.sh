33fcfb3f1d57cf2969eddcd39d0452859d209184
tsalla <tilak.04@yahoo.com>
2022-01-26 13:39:09 +0530

    Updating shell-scripting

--- roboshop-project/components/mongodb.sh
+++ roboshop-project/components/mongodb.sh
@@ -1,2 +1,33 @@
+
+
+
+#1. Setup MongoDB repos.
+#
+#```bash
+#curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+#```
+#
+#1. Install Mongo & Start Service.
+#
+#```bash
+## yum install -y mongodb-org
+## systemctl enable mongod
+## systemctl start mongod
+#
+#```
+#
+#1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in config file
+#
+#Config file: `/etc/mongod.conf`
+#
+#then restart the service
+#
+#```bash
+## systemctl restart mongod
+#
+#```
 source components/common.sh
+
+echo "Download Mpngodb repo file"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
 
