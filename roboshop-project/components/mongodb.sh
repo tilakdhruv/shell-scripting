6f1f626b1beca29319f0a17cff50b73188f46195
tsalla <tilak.04@yahoo.com>
2022-02-03 19:54:01 +0530

    Updating shell-scripting

--- roboshop-project/components/mongodb.sh
+++ roboshop-project/components/mongodb.sh
@@ -1,45 +1,35 @@
-
-
-
-#1. Setup MongoDB repos.
-#
-#```bash
-#curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
-#```
-#
-#1. Install Mongo & Start Service.
-#
-#```bash
-## yum install -y mongodb-org
-## systemctl enable mongod
-## systemctl start mongod
-#
-#```
-#
-#1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in config file
-#
-#Config file: `/etc/mongod.conf`
-#
-#then restart the service
-#
-#```bash
-## systemctl restart mongod
-#
-#```
 source components/common.sh
 
-echo "Download Mpngodb repo file"
+echo "Download MongoDb repo file"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
+STAT $?
 
-echo "Install mongodb"
+echo "Install MongoDB"
 yum install -y mongodb-org &>>$LOG_FILE
+STAT $?
 
-echo "Update mongodb config file"
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>$LOG_FILE
+echo "Update MongoDB Config file"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf  &>>$LOG_FILE
+STAT $?
 
 echo "Start Database"
-systemctl enable mongod &>>$LOG_FILE
-systemctl start mongod &>>$LOG_FILE
+systemctl enable mongod  &>>$LOG_FILE && systemctl start mongod &>>$LOG_FILE
+STAT $?
 
+echo "Download Schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"  &>>$LOG_FILE
+STAT $?
+
+echo "Extract Schema"
+cd /tmp/
+unzip -o mongodb.zip  &>>$LOG_FILE
+STAT $?
+
+echo "Load Schema"
+cd mongodb-main
+for app in catalogue users ; do
+  mongo < ${app}.js &>>$LOG_FILE
+done
+STAT $?
 
 
