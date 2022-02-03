c24b445ffb24b8c85fac72d8acd8efb77d9156b2
tsalla <tilak.04@yahoo.com>
2022-02-03 18:28:18 +0530

    Updating shell-scripting

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -1,3 +1,17 @@
+nes (13 sloc)  378 Bytes
+
 #!/bin/bash
 
-bash components/$1.sh
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+  echo -e "\e[1;31m You should be root user to execute this script ..\e[0m"
+  exit 1
+fi
+
+if [ -f components/$1.sh ]; then
+  bash components/$1.sh
+else
+  echo -e "\e[1;31mInvalid Input\e[0m"
+  echo -e "\e[1;33mAvailable Inputs - frontend|mongodb|catalogue|redis|user|cart|mysql|shipping|payment|rabbitmq|dispatch\e[0m"
+  exit 1
+fi
